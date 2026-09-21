134e4745716010a6a7e04a387ae7b3ec2a0a3988
Yan <yanzhaocd@gmail.com>
2026-09-21 16:04:52 -0700

    there is 1 test left on Wave 2.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,10 @@
+import uuid 
 class Item:
-    pass
+    def __init__(self, id=None):
+        self.id = id
+        if id is None:
+            self.id = uuid.uuid4().int
+        
+
+    def get_category(self):
+        return "Item"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -14,3 +14,10 @@ class Vendor:
             return item
         else: 
             return None
+
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+            else:
+                return None
