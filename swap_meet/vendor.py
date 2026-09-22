82b526ac0698ad77c7464a754c2609346c3ca2dc
Yan <yanzhaocd@gmail.com>
2026-09-22 15:12:39 -0700

    warped up wave 2, made some changes based on Ansel's comments and finished wave 3.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -9,15 +9,27 @@ class Vendor:
         return item
 
     def remove(self, item):
-        if item in self.inventory:
-            self.inventory.remove(item)
-            return item
-        else: 
+        if item not in self.inventory:
             return None
+
+        self.inventory.remove(item)
+        return item
+        
 
     def get_by_id(self, id):
         for item in self.inventory:
             if item.id == id:
                 return item
-            else:
-                return None
+        return None
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+
+        my_giveaway = self.remove(my_item)
+        other_vendor.inventory.append(my_giveaway)
+
+        friend_giveaway = other_vendor.remove(their_item)
+        self.inventory.append(friend_giveaway)
+
+        return True
