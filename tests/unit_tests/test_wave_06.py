071bbeb7bc7217aa168952b44da5f6b82c052572
Najma Shanle <najmashanle22@gmail.com>
2026-09-24 15:12:52 -0700

    remove duplicate asserts

--- tests/unit_tests/test_wave_06.py
+++ tests/unit_tests/test_wave_06.py
@@ -303,12 +303,4 @@ def test_swap_best_by_category_no_other_match_is_false():
     # - That result is falsy
     # - That tai and jesse's inventories are the correct length
     # - That all the correct items are in tai and jesse's inventories
-    assert not result
-    assert len(tai.inventory) == 3
-    assert len(jesse.inventory) == 3
-    assert item_a in tai.inventory
-    assert item_b in tai.inventory
-    assert item_c in tai.inventory
-    assert item_d in jesse.inventory
-    assert item_e in jesse.inventory
-    assert item_f in jesse.inventory
+    
